099627ec7f1c83c7357878fedb27807fcfb6c317
manvyas <manishv0993@gmail.com>
2015-07-07 11:04:19 +0530

    alphanot

---
+++ src/main/java/com/muktalabs/weatheranalysis/monthly/mapred/RdiCalculations.java
@@ -0,0 +1,51 @@
+package com.muktalabs.weatheranalysis.daily.mapred;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.muktalabs.weatheranalysis.daily.DailyWeather;
+import com.muktalabs.weatheranalysis.monthly.MonthlyWeather;
+import com.muktalabs.weatheranalysis.monthly.MonthlyWeatherHbaseOperations;
+
+
+public class RdiCalculations {
+	
+	
+	
+	public static void main(String args[])
+	{
+	  float ppt =calculatealphanot(420710,2014);
+	   System.out.println( "Yearly ppt" + ppt);
+	}
+
+	public static float calculatealphanot(int stationcode,int year)
+	{   int i = 0;
+		// i need a array list here for all the months from monthly temperature //
+		List <MonthlyWeather> rd= new ArrayList<MonthlyWeather>();
+		float YearlyPpt=0;
+		try {
+			rd = MonthlyWeatherHbaseOperations.get(stationcode, year);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+   for(MonthlyWeather rm:rd)
+		{ 
+	   float g=rm.getPrecipitation();
+	   System.out.println(g);
+		YearlyPpt=YearlyPpt+rm.getPrecipitation();
+	  	i++;
+	  	}
+		float pet = 1;
+		//Retrieve PET from method (for that year . for that station)
+		float alphanot = YearlyPpt/pet ;
+		//Calcuate standard rdi
+		
+	return alphanot;	
+	}
+}
+
+	
+	
+
